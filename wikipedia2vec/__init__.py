de3fe1a94bdf527053b06fc68b128a10bb668d3e
Yiwen Wei <yw2903@columbia.edu>
2019-07-31 16:13:34 +0000

    gitignore part II

--- wikipedia2vec/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# License: Apache License 2.0
-
-from .dictionary import Dictionary
-from .wikipedia2vec import Wikipedia2Vec
--- wikipedia2vec/utils/sentence_detector/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# License: Apache License 2.0
-
-def get_sentence_detector(name, language=None):
-    if name == 'icu':
-        from .icu_sentence_detector import ICUSentenceDetector
-        return ICUSentenceDetector(language)
-    else:
-        raise NotImplementedError()
